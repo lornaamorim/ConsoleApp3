9ebe10b0e64665bcd1afeb1fbee78e0ecfd62a42
Usuario <Usuario@CPD03-M08>
2024-02-27 11:18:47 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor litro do combustível: ");
+             double n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a distância percorrida: ");
+             double n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o consumo: ");
+             double n3 = int.Parse(Console.ReadLine());
+
+              double resultado = n1 / n2 * n3;
+            Console.WriteLine("O gasto total da viagem foi " + resultado);
+
+
+                Console.ReadKey();
+
+        }
+    }
+}
